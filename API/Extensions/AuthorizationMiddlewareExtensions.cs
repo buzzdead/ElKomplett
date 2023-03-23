d64503a08cb49bdacd987595ace8287f339540de
Sigmund.Volden <sigmund.volden@student.uib.no>
2023-03-23 17:07:52 +0100

    Added authorization middleware for test admin

---
+++ API/Extensions/AuthorizationMiddlewareExtensions.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Builder;
+
+public static class AuthorizationMiddlewareExtensions
+{
+    public static IApplicationBuilder UseAuthorizationMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<AuthorizationMiddleware>();
+    }
+}
