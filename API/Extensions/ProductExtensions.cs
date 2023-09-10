fdaedc2feffccd5de077497cb8ba16a60080c7f6
buzzdead <sigmundvolden@gmail.com>
2023-09-10 13:11:43 +0200

    filter fix 2

--- API/Extensions/ProductExtensions.cs
+++ API/Extensions/ProductExtensions.cs
@@ -27,23 +27,21 @@ namespace API.Extensions
 
         public static IQueryable<Product> Filter(this IQueryable<Product> query, string producers, string productTypes, int categoryId, EntityMappingCache mappingCache)
         {
-            var brandList = new List<string>();
-            var typeList = new List<string>();
             var cId = categoryId != 0;
             if (categoryId != 0) query = query.Where(p => p.categoryId == categoryId);
             var (producerNames, productTypeNames) = mappingCache.GetProducersAndProductTypes(categoryId);
             if (!string.IsNullOrEmpty(producers))
             {
-                var producerNameList = producers.Split(",").Where(producerNames.Contains).ToList();
+                var producerNameList = cId ? producers.Split(",").Where(producerNames.Contains).ToList() : producers.Split(",").ToList();
                 var producerIds = producerNameList.Select(mappingCache.GetProducerIdFromName).ToList();
                 query = query.Where(p => producerNameList.Count == 0 || producerIds.Contains(p.Producer.Id));
             }
 
             if (!string.IsNullOrEmpty(productTypes))
             {
-                typeList.AddRange(productTypes.Split(",").Where(productTypeNames.Contains).ToList());
-                var productTypeIds = typeList.Select(mappingCache.GetProductTypeIdFromName).ToList();
-                query = query.Where(p => typeList.Count == 0 || productTypeIds.Contains(p.ProductType.Id));
+                var productTypeList = cId ? productTypes.Split(",").Where(productTypeNames.Contains).ToList() : productTypes.Split(",").ToList();
+                var productTypeIds = productTypeList.Select(mappingCache.GetProductTypeIdFromName).ToList();
+                query = query.Where(p => productTypeList.Count == 0 || productTypeIds.Contains(p.ProductType.Id));
             }
             return query;
         }
